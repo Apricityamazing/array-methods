b2545c5795a6485132c8388951b5e25a995a6841
Apricityamazing <apricityamazing@gmail.com>
2025-07-30 10:14:59 -0500

    Split the string into an array

---
+++ camelize.js
@@ -0,0 +1,11 @@
+// Turn strings seperated by dashes into camel-cased strings
+// Ex. border-left-width --> borderLeftWidth
+// 1. Turn string inputed into function camelize() into an array spliting words by each dash
+// 2. Capitalize first letter of every word after the first word
+// 3. Join the array back together into a camelized string, then return the string.
+function camelize(str) {
+    // Splits string into an array
+    let arr = str.split('-');
+    return arr;
+}
+console.log(camelize("border-left-width"));
