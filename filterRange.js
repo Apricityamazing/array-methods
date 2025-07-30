541c117cd4b0ffb61801eb1ab27ee02aa9e2fb65
Apricityamazing <apricityamazing@gmail.com>
2025-07-30 11:15:04 -0500

    Wrote pseudocode

---
+++ filterRange.js
@@ -0,0 +1,6 @@
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+// 1. Make a function that accepts 3 parameters, an array, and a minimum and maximum value
+// 2. Declare a new array with the value of the inputted array.
+// 3. Check all numbers in the new array to see if they are greater than or equal to the minimum AND less than and equal to the maximum
+// 4. If numbers don't satisfy those two, remove them from the new array.
+// 5. Return the new array
